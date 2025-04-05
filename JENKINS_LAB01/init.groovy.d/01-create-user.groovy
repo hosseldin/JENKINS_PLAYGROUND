379f014704eb5eae658d47c0946558256821ce9b
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2025-04-05 22:04:41 +0200

    added the script for the 1st step

---
+++ JENKINS_LAB01/init.groovy.d/01-create-user.groovy
@@ -0,0 +1,18 @@
+// Import required Jenkins core classes
+import jenkins.model.*
+import hudson.security.*
+
+// Get the Jenkins instance
+def instance = Jenkins.getInstance()
+
+// Create a new security realm that allows managing users inside Jenkins (not external like LDAP)
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+
+// Create a new user with username 'devuser' and password 'password123'
+hudsonRealm.createAccount("devuser", "password123")
+
+// Set this realm (user system) on Jenkins
+instance.setSecurityRealm(hudsonRealm)
+
+// Save the configuration
+instance.save()
