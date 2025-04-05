3bc860357e8d4c2fb3bc754e888124d80ab1949c
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2025-04-05 22:05:57 +0200

    added the script for the 3rd step to be executed

---
+++ JENKINS_LAB01/init.groovy.d/03-create-job.groovy
@@ -0,0 +1,33 @@
+// Import Jenkins core classes
+import jenkins.model.*
+import hudson.model.*
+import hudson.tasks.Shell
+import hudson.plugins.git.*
+
+// Get the main Jenkins instance
+def jenkins = Jenkins.getInstance()
+
+// Define a job name
+def jobName = "hello-world-job"
+
+// Only create the job if it doesn't already exist
+if (jenkins.getItem(jobName) == null) {
+    
+    // Create a new Freestyle project with the defined name
+    def job = jenkins.createProject(FreeStyleProject, jobName)
+    
+    // Configure Git source (replace with your actual repo URL)
+    job.setScm(new GitSCM("https://github.com/YOUR_USERNAME/YOUR_PRIVATE_REPO.git"))
+    
+    // Define shell commands to run in the build step
+    def shellBuilder = new Shell("""
+        mkdir hello-dir
+        echo "Hello World" > hello-dir/hello.txt
+    """)
+    
+    // Add the shell builder to the job's build steps
+    job.getBuildersList().add(shellBuilder)
+
+    // Save the job configuration
+    job.save()
+}
