03856478060763cb8d1a4803489160d26d59c2f6
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2025-04-11 23:36:34 +0200

    changed the useranme

--- JENKINS_LAB01/init.groovy.d/01-create-user.groovy
+++ JENKINS_LAB01/init.groovy.d/01-create-user.groovy
@@ -9,7 +9,7 @@ def instance = Jenkins.getInstance()
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 
 // Create a new user with username 'hosa-docker' and password '1234'
-hudsonRealm.createAccount("hosa-docker", "1234")
+hudsonRealm.createAccount("hosa01", "1234")
 
 // Set this realm (user system) on Jenkins
 instance.setSecurityRealm(hudsonRealm)
